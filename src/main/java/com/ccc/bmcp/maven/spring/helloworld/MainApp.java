02af39a61fb32d058c43dee577b185dd144ae0b1
David Chang <chechiachang999@gmail.com>
2015-08-18 17:38:36 +0800

    Initial commit

---
+++ src/main/java/com/ccc/bmcp/maven/spring/helloworld/MainApp.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ccc.bmcp.maven.spring.helloworld;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author davidchang
+ */
+public class MainApp {
+    public static void main(String[] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+        
+        helloWorld.setMessage("I'm helloWorld A");
+        helloWorld.getMessage();
+        
+        HelloWorld helloWorldB = (HelloWorld)context.getBean("helloWorld");
+        helloWorldB.getMessage();
+
+    }
+    
+}
